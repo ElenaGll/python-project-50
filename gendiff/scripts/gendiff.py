c8292320ed611304b0a12a9f8ce2808988340c59
Elena Donyakina <hellengoll@gmail.com>
2023-01-20 00:32:59 +0300

    added gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python3
+
+
 import argparse
 
 
 def main():
 	parser = argparse.ArgumentParser(prog='gendiff', description='Compares two configuration files and shows a difference.')
-	parser.add_argument("first_file")
-	parser.add_argument("second_file")
+	parser.add_argument('first_file')
+	parser.add_argument('second_file')
+	parser.add_argument('-f', '--format', type=str, help='set format of output')
 	args = parser.parse_args()
 	print(args.first_file, args.second_file)
 
